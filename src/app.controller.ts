3ac3b8a559aa1e0b1856b0d20c6cec936fd1387a
DC <cuong.nl5.kma@gmail.com>
2022-11-07 15:46:01 +0700

    feat: health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common'
-import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @Get('/healthz')
+  healthCheck() {
+    const { npm_package_version, SERVICE_VERSION } = process.env
+    return {
+      version: `v${SERVICE_VERSION || npm_package_version}`,
+    }
   }
 }
