4e2434e0cd7389380d5a85b8c0f97b7c1c72e347
DC <cuong.nl5.kma@gmail.com>
2022-11-10 15:18:43 +0700

    feat: add typeorm custom repository

---
+++ src/modules/typeorm-ex.module.ts
@@ -0,0 +1,33 @@
+import { TYPEORM_EX_CUSTOM_REPOSITORY } from '@decorators/typeorm-ex.decorator'
+import { DynamicModule, Provider } from '@nestjs/common'
+import { getDataSourceToken } from '@nestjs/typeorm'
+import { DataSource } from 'typeorm'
+
+export class TypeOrmExModule {
+  public static forCustomRepository<T extends new (...args: any[]) => any>(repositories: T[]): DynamicModule {
+    const providers: Provider[] = []
+
+    for (const repository of repositories) {
+      const entity = Reflect.getMetadata(TYPEORM_EX_CUSTOM_REPOSITORY, repository)
+
+      if (!entity) {
+        continue
+      }
+
+      providers.push({
+        inject: [getDataSourceToken()],
+        provide: repository,
+        useFactory: (dataSource: DataSource): typeof repository => {
+          const baseRepository = dataSource.getRepository<any>(entity)
+          return new repository(baseRepository.target, baseRepository.manager, baseRepository.queryRunner)
+        },
+      })
+    }
+
+    return {
+      exports: providers,
+      module: TypeOrmExModule,
+      providers,
+    }
+  }
+}
