69e5e9a249b4bc9106de0f12333cc892f454a1eb
DC <cuong.nl5.kma@gmail.com>
2022-11-14 22:29:50 +0700

    feat: add e2e testing when pushing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,22 +1,9 @@
-import { INestApplication } from '@nestjs/common'
-import { Test, TestingModule } from '@nestjs/testing'
 import * as request from 'supertest'
 
-import { AppModule } from './../src/app.module'
-
 describe('AppController (e2e)', () => {
-  let app: INestApplication
+  const apiURL = process.env.API_URL
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile()
-
-    app = moduleFixture.createNestApplication()
-    await app.init()
-  })
-
-  it('/healthz (GET)', () => {
-    return request(app.getHttpServer()).get('/healthz').expect(200)
+  it('/v1/healthz (GET)', () => {
+    return request(apiURL).get('/v1/healthz').expect(200)
   })
 })
