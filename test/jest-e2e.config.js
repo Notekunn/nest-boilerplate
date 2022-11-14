0d038daf6f456bc5aef076b121fbcd91db98093c
Restyled.io <commits@restyled.io>
2022-11-14 16:13:47 +0000

    Restyled by clang-format

--- test/jest-e2e.config.js
+++ test/jest-e2e.config.js
@@ -1,14 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { pathsToModuleNameMapper } = require('ts-jest')
-const { compilerOptions } = require('../tsconfig.json')
+const {pathsToModuleNameMapper} = require('ts-jest')
+const {compilerOptions} = require('../tsconfig.json')
 
 module.exports = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: '.',
-  testEnvironment: 'node',
-  testRegex: '.e2e-spec.ts$',
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
+  moduleFileExtensions : [ 'js', 'json', 'ts' ],
+  rootDir : '.',
+  testEnvironment : 'node',
+  testRegex : '.e2e-spec.ts$',
+  transform : {
+    '^.+\\.(t|j)s$' : 'ts-jest',
   },
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/..' }),
+  moduleNameMapper :
+      pathsToModuleNameMapper(compilerOptions.paths, {prefix : '<rootDir>/..'}),
 }
