9757ab6059d1c7e4cf6acb8b48e619e06fb9b922
DC <cuong.nl5.kma@gmail.com>
2023-01-15 23:46:34 +0700

    fix: status code when wrong email and password

--- src/modules/auth/cqrs/commands/handler/login-by-email.handler.ts
+++ src/modules/auth/cqrs/commands/handler/login-by-email.handler.ts
@@ -1,5 +1,5 @@
 import { GetUserByEmailQuery } from '@modules/users/cqrs/queries/impl/get-user-by-email.query'
-import { NotFoundException } from '@nestjs/common'
+import { BadRequestException } from '@nestjs/common'
 import { CommandHandler, ICommandHandler, QueryBus } from '@nestjs/cqrs'
 import { UtilService } from '@shared/utils.service'
 
@@ -13,13 +13,13 @@ export class LoginByEmailCommandHandler implements ICommandHandler<LoginByEmailC
     const { email, password } = _command
     const user = await this.queryBus.execute(new GetUserByEmailQuery(email))
     if (!user) {
-      throw new NotFoundException('error.userNotFound')
+      throw new BadRequestException('error.userPasswordNotMatching')
     }
 
     const isPasswordMatching = await UtilService.validateHash(password, user.password)
 
     if (!isPasswordMatching) {
-      throw new NotFoundException('error.passwordNotCorrect')
+      throw new BadRequestException('error.userPasswordNotMatching')
     }
 
     return user
