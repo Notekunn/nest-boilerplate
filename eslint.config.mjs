5303138df6c502a4f115e5dbd566889d45cf6663
DC <cuong.nl5.kma@gmail.com>
2025-03-29 16:58:11 +0700

    fix: root dir

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,63 +1,69 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import typescriptEslintEslintPlugin from "@typescript-eslint/eslint-plugin";
-import _import from "eslint-plugin-import";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import { fixupPluginRules } from "@eslint/compat";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin'
+import _import from 'eslint-plugin-import'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import { fixupPluginRules } from '@eslint/compat'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
 
-export default defineConfig([globalIgnores(["**/.eslintrc.js"]), {
-    extends: compat.extends("plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"),
+export default defineConfig([
+  globalIgnores(['**/.eslintrc.js']),
+  {
+    extends: compat.extends('plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'),
 
     plugins: {
-        "@typescript-eslint": typescriptEslintEslintPlugin,
-        import: fixupPluginRules(_import),
-        "simple-import-sort": simpleImportSort,
+      '@typescript-eslint': typescriptEslintEslintPlugin,
+      import: fixupPluginRules(_import),
+      'simple-import-sort': simpleImportSort,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.jest,
-        },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
 
-        parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'module',
 
-        parserOptions: {
-            project: "tsconfig.json",
-            tsconfigRootDir: "/Users/notekunn/workspaces/side-project/pet-project/boiler-plate/nest-boilerplate",
-        },
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
 
     rules: {
-        "@typescript-eslint/interface-name-prefix": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-explicit-any": "off",
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
 
-        "@typescript-eslint/no-unused-vars": ["warn", {
-            argsIgnorePattern: "^_",
-            varsIgnorePattern: "^_",
-        }],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
 
-        "prettier/prettier": ["error"],
-        "simple-import-sort/imports": "error",
-        "simple-import-sort/exports": "error",
-        "import/no-default-export": "warn",
-        "@typescript-eslint/no-empty-function": "off",
+      'prettier/prettier': ['error'],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'import/no-default-export': 'warn',
+      '@typescript-eslint/no-empty-function': 'off',
     },
-}]);
+  },
+])
